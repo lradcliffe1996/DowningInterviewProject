998aaf78f6592575b93ad4d563c0102c7095f79b
Liam Radcliffe <liamradcliffe@outlook.com>
2024-08-11 15:39:40 +0100

    created controller

---
+++ DowningInterviewProject.Server/Controllers/CompaniesController.cs
@@ -0,0 +1,71 @@
+﻿using DowningInterviewProject.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DowningInterviewProject.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ILogger<CompaniesController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public CompaniesController(ILogger<CompaniesController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<Company> GetCompanies() 
+        {
+            //List<Company> companies = new List<Company>();
+            //string query = @"SELECT * FROM Companies";
+            //DataTable table = new DataTable();
+            //string sqlDataSource = _configuration.GetConnectionString("AppConn");
+            //SqlDataReader myReader;
+            //using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            //{
+            //    myCon.Open();
+            //    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            //    {
+            //        myReader = myCommand.ExecuteReader();
+            //        table.Load(myReader);
+
+            //        myReader.Close();
+            //        myCon.Close();
+            //    }
+            //}
+
+            //foreach (DataRow row in table.Rows) 
+            //{
+            //    companies.Add(
+            //        new Company
+            //        {
+            //            Id = (int)row["Id"],
+            //            CompanyName = (string)row["CompanyName"],
+            //            CreatedDate = (DateTime)row["CreatedDate"],
+            //            Code = (string)row["Code"],
+            //            SharePrice = (decimal)row["SharePrice"]
+            //        }
+            //    );
+            //}
+
+            //return companies;
+
+            return new List<Company> {
+                new Company
+                {
+                    Id = 0,
+                    CompanyName = "Test",
+                    CreatedDate = DateTime.Now,
+                    Code = "TS",
+                    SharePrice = (decimal?)3_000.5
+                }
+            };
+        }
+    }
+}
---
+++ DowningInterviewProject.Server/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace DowningInterviewProject.Server.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string? CompanyName { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? Code {  get; set; }
+        public decimal? SharePrice {  get; set; }
+    }
+}
