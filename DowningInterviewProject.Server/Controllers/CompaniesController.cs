abab052f38f3d3c62ffac8463ac3a64241f79488
Liam Radcliffe <liamradcliffe@outlook.com>
2024-08-11 16:27:38 +0100

    got multiple endpoints working

--- DowningInterviewProject.Server/Controllers/CompaniesController.cs
+++ DowningInterviewProject.Server/Controllers/CompaniesController.cs
@@ -7,7 +7,7 @@ using System.Data.SqlClient;
 namespace DowningInterviewProject.Server.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class CompaniesController : ControllerBase
     {
         private readonly ILogger<CompaniesController> _logger;
@@ -19,53 +19,54 @@ namespace DowningInterviewProject.Server.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet]
-        public IEnumerable<Company> GetCompanies() 
+        [HttpGet(Name = "GetCompanies")]
+        public ActionResult<IEnumerable<Company>> Get() 
         {
-            //List<Company> companies = new List<Company>();
-            //string query = @"SELECT * FROM Companies";
-            //DataTable table = new DataTable();
-            //string sqlDataSource = _configuration.GetConnectionString("AppConn");
-            //SqlDataReader myReader;
-            //using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            //{
-            //    myCon.Open();
-            //    using (SqlCommand myCommand = new SqlCommand(query, myCon))
-            //    {
-            //        myReader = myCommand.ExecuteReader();
-            //        table.Load(myReader);
-
-            //        myReader.Close();
-            //        myCon.Close();
-            //    }
-            //}
-
-            //foreach (DataRow row in table.Rows) 
-            //{
-            //    companies.Add(
-            //        new Company
-            //        {
-            //            Id = (int)row["Id"],
-            //            CompanyName = (string)row["CompanyName"],
-            //            CreatedDate = (DateTime)row["CreatedDate"],
-            //            Code = (string)row["Code"],
-            //            SharePrice = (decimal)row["SharePrice"]
-            //        }
-            //    );
-            //}
-
-            //return companies;
-
-            return new List<Company> {
-                new Company
+            List<Company> companies = new List<Company>();
+            string query = @"SELECT * FROM Companies";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
-                    Id = 0,
-                    CompanyName = "Test",
-                    CreatedDate = DateTime.Now,
-                    Code = "TS",
-                    SharePrice = (decimal?)3_000.5
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
                 }
-            };
+            }
+
+            foreach (DataRow row in table.Rows)
+            {
+                companies.Add(
+                    new Company
+                    {
+                        Id = (int)row["Id"],
+                        CompanyName = (string)row["CompanyName"],
+                        CreatedDate = (DateTime)row["CreatedDate"],
+                        Code = (string)row["Code"],
+                        SharePrice = (decimal)row["SharePrice"]
+                    }
+                );
+            }
+
+            return companies;
+
+            //var companies = new List<Company> {
+            //    new Company
+            //    {
+            //        Id = 0,
+            //        CompanyName = "Test",
+            //        CreatedDate = DateTime.Now,
+            //        Code = "TS",
+            //        SharePrice = (decimal?)3_000.5
+            //    }
+            //};
+            //return companies;
         }
     }
 }
--- downinginterviewproject.client/src/app/app.component.ts
+++ downinginterviewproject.client/src/app/app.component.ts
@@ -8,6 +8,14 @@ interface WeatherForecast {
   summary: string;
 }
 
+interface Companies {
+  id: number,
+  companyName?: string,
+  createdDate?: Date,
+  code?: string,
+  sharePrice?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,11 +23,13 @@ interface WeatherForecast {
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
+  public companies: Companies[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getForecasts();
+    this.getCompanies();
   }
 
   getForecasts() {
@@ -31,6 +41,16 @@ export class AppComponent implements OnInit {
         console.error(error);
       }
     );
+  }
+
+  public getCompanies(): void {
+    this.http.get<Companies[]>('/companies').subscribe({
+      next: (results) => {
+        this.companies = results;
+        console.log(this.companies);
+      },
+      error: (error) => { console.error(error); }
+    })
   }
 
   title = 'downinginterviewproject.client';
